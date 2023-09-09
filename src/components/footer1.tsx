d05a18344d43685ebc7cf9b3e6e54babd205da3a
Manav Malhotra <manavmalhotrafrnd4u@gmail.com>
2023-09-09 19:47:48 +0530

    minor changes

--- src/components/footer1.tsx
+++ src/components/footer1.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import logo from "../../public/logo.svg";
 import { Mail, Phone } from "lucide-react";
@@ -18,10 +19,21 @@ import {
   BsWhatsapp,
 } from "react-icons/bs";
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Separator } from "./ui/separator";
+
 export default function Footer1() {
   return (
     <div className="bg-[#f7f7fe]">
-      <div className="container px-5 py-10 mx-auto">
+      <div className="container px-24 py-10 mx-auto">
         {/* Footer Top */}
         <div className="border-b border-[#24255e80] pb-6">
           <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between">
@@ -58,7 +70,7 @@ export default function Footer1() {
         </div>
         {/* Footer Middle */}
         <div className="flex py-8 lg:flex lg:items-start lg:justify-between">
-          <div className="w-full pr-0 lg:w-1/2 lg:pr-20">
+          <div className="w-full pr-0 lg:w-2/5 lg:pr-20">
             <p className="text-base leading-7 text-black">
               ScaleneWorks is a global talent solutions organization with over a
               decade of experience. We specialize in providing customized HR
@@ -67,7 +79,7 @@ export default function Footer1() {
               client-centric focus, we deliver exceptional results for MNCs and
               job seekers alike.
             </p>
-            <div className="flex mt-6 space-x-4">
+            <div className="flex space-x-4 my-14">
               <BsFacebook className="w-6 h-6 text-[#5557DF]" />
               <BsTwitter className="w-6 h-6 text-[#5557DF]" />
               <BsLinkedin className="w-6 h-6 text-[#5557DF]" />
@@ -133,86 +145,179 @@ export default function Footer1() {
               </AccordionItem>
             </Accordion>
           </div>
-          <div className="flex-1 mt-8 lg:mt-0 lg:w-1/2 ">
+          <div className="flex-1 mt-8 lg:mt-0 lg:w-3/5">
+            <Table>
+              <TableBody>
+                {/* ITEM #1 */}
+                <TableRow
+                  style={{
+                    borderBottom: "none",
+                  }}
+                >
+                  <TableCell rowSpan={2} style={{ verticalAlign: "top" }}>
+                    Products
+                  </TableCell>
+                  <TableCell>Human Capital Management</TableCell>
+                  <TableCell>Payroll and Workflow management</TableCell>
+                </TableRow>
+                <TableRow
+                  style={{
+                    borderBottom: "none",
+                  }}
+                >
+                  <TableCell>Talent Acquisition</TableCell>
+                  <TableCell>Talent Management</TableCell>
+                </TableRow>
+                {/* ITEM #2 */}
+                <TableRow
+                  style={{
+                    borderBottom: "none",
+                  }}
+                >
+                  <TableCell rowSpan={3} style={{ verticalAlign: "top" }}>
+                    Modules
+                  </TableCell>
+                  <TableCell>Recruit</TableCell>
+                  <TableCell>Onboarding</TableCell>
+                </TableRow>
+                <TableRow
+                  style={{
+                    borderBottom: "none",
+                  }}
+                >
+                  <TableCell>Core HR</TableCell>
+                  <TableCell>Payroll</TableCell>
+                </TableRow>
+                <TableRow
+                  style={{
+                    borderBottom: "none",
+                  }}
+                >
+                  <TableCell>Human Capital Management</TableCell>
+                  <TableCell>Employee Verification</TableCell>
+                </TableRow>
+                {/* ITEM #3 */}
+                <TableRow
+                  style={{
+                    borderBottom: "none",
+                  }}
+                >
+                  <TableCell style={{ verticalAlign: "top" }}>
+                    Industries
+                  </TableCell>
+                  <TableCell>Banking & Financial Services</TableCell>
+                  <TableCell>Healthcare</TableCell>
+                </TableRow>
+                {/* ITEM #4 */}
+                <TableRow
+                  style={{
+                    borderBottom: "none",
+                  }}
+                >
+                  <TableCell rowSpan={2} style={{ verticalAlign: "top" }}>
+                    Company
+                  </TableCell>
+                  <TableCell>Human Capital Management</TableCell>
+                  <TableCell>Payroll and Workflow management</TableCell>
+                </TableRow>
+                <TableRow
+                  style={{
+                    borderBottom: "none",
+                  }}
+                >
+                  <TableCell>Talent Acquisition</TableCell>
+                  <TableCell>Talent Management</TableCell>
+                </TableRow>
+                {/* ITEM #5 */}
+                <TableRow
+                  style={{
+                    borderBottom: "none",
+                  }}
+                >
+                  <TableCell rowSpan={2} style={{ verticalAlign: "top" }}>
+                    Products
+                  </TableCell>
+                  <TableCell>Human Capital Management</TableCell>
+                  <TableCell>Payroll and Workflow management</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div>
+          {/* <div className="flex-1 mt-8 lg:mt-0 lg:w-1/2 ">
             <div className="flex flex-col space-y-4">
-              {/* ITEM #1 */}
-              <div className="flex flex-row space-x-3">
+              <div className="flex flex-row space-x-3 text-left">
                 <h3>Products</h3>
-                <div className="flex flex-col items-start ">
+                <div className="flex flex-col items-start space-y-2">
                   <h3>Human Capital Management</h3>
                   <h3>Payroll and Workflow management</h3>
                 </div>
-                <div className="flex flex-col items-start ">
+                <div className="flex flex-col items-start space-y-2">
                   <h3>Talent Acquisition</h3>
                   <h3>Talent Management</h3>
                 </div>
               </div>
-              <div className="flex flex-row space-x-3">
-                <h3>Products</h3>
-                <div className="flex flex-col items-start ">
+              <div className="flex flex-row space-x-3 text-left">
+                <h3>Modules</h3>
+                <div className="flex flex-col items-start space-y-2">
+                  <h3>Recruit</h3>
+                  <h3>Onboarding</h3>
+                  <h3>Core HR</h3>
+                  <h3>Payroll</h3>
+                </div>
+                <div className="flex flex-col items-start space-y-2">
+                  <h3>Human Capital Management</h3>
+                  <h3>Employee Verification</h3>
+                  <h3>Leave & Attendence</h3>
+                </div>
+              </div>
+              <div className="flex flex-row space-x-3 text-left">
+                <h3>Industries</h3>
+                <div className="flex flex-col items-start space-y-2">
+                  <h3>Banking & Financial Services</h3>
+                  <h3>Healthcare</h3>
+                </div>
+                <div className="flex flex-col items-start space-y-2">
                   <h3>Human Capital Management</h3>
                   <h3>Payroll and Workflow management</h3>
                 </div>
-                <div className="flex flex-col items-start ">
+              </div>
+              <div className="flex flex-row space-x-3 text-left">
+                <h3>Company</h3>
+                <div className="flex flex-col items-start space-y-2">
+                  <h3>Human Capital Management</h3>
+                  <h3>Payroll and Workflow management</h3>
+                </div>
+                <div className="flex flex-col items-start space-y-2">
                   <h3>Talent Acquisition</h3>
                   <h3>Talent Management</h3>
                 </div>
               </div>
-              <div className="flex flex-row space-x-3">
+              <div className="flex flex-row space-x-3 text-left">
                 <h3>Products</h3>
-                <div className="flex flex-col items-start ">
+                <div className="flex flex-col items-start space-y-2">
                   <h3>Human Capital Management</h3>
                   <h3>Payroll and Workflow management</h3>
                 </div>
-                <div className="flex flex-col items-start ">
-                  <h3>Talent Acquisition</h3>
-                  <h3>Talent Management</h3>
-                </div>
-              </div>
-              <div className="flex flex-row space-x-3">
-                <h3>Products</h3>
-                <div className="flex flex-col items-start ">
-                  <h3>Human Capital Management</h3>
-                  <h3>Payroll and Workflow management</h3>
-                </div>
-                <div className="flex flex-col items-start ">
-                  <h3>Talent Acquisition</h3>
-                  <h3>Talent Management</h3>
-                </div>
-              </div>
-              <div className="flex flex-row space-x-3">
-                <h3>Products</h3>
-                <div className="flex flex-col items-start ">
-                  <h3>Human Capital Management</h3>
-                  <h3>Payroll and Workflow management</h3>
-                </div>
-                <div className="flex flex-col items-start ">
+                <div className="flex flex-col items-start space-y-2">
                   <h3>Talent Acquisition</h3>
                   <h3>Talent Management</h3>
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
+        <Separator />
         {/* Footer Bottom */}
-        <div className="py-4 border-t border-[#24255e80] flex flex-col lg:flex-row lg:items-center lg:justify-between">
-          <p className="text-[#000000bf] text-sm font-normal lg:w-2/3">
+        <div className="flex flex-col py-4 lg:flex-row lg:items-center lg:justify-between">
+          <p className="text-sm font-normal lg:w-2/3">
             © 2023 Scalene Works, All rights reserved
           </p>
-          <div className="flex mt-4 space-x-8 lg:mt-0">
-            <div className="text-[#000000bf] text-sm font-normal">Brand</div>
-            <div className="text-[#000000bf] text-sm font-normal">
-              Customer Support
-            </div>
-            <div className="text-[#000000bf] text-sm font-normal">
-              Cookie Policy
-            </div>
-            <div className="text-[#000000bf] text-sm font-normal">
-              Privacy & GDPR Policy
-            </div>
-            <div className="text-[#000000bf] text-sm font-normal">
-              Security Resources
-            </div>
+          <div className="flex w-full mt-4 space-x-8 lg:mt-0">
+            <div className="text-sm font-normal">Brand</div>
+            <div className="text-sm font-normal">Customer Support</div>
+            <div className="text-sm font-normal">Cookie Policy</div>
+            <div className="text-sm font-normal">Privacy & GDPR Policy</div>
+            <div className="text-sm font-normal">Security Resources</div>
           </div>
         </div>
       </div>
