47723302eff33a8147652a65e80c331dad74d1d2
Manav Malhotra <manavmalhotrafrnd4u@gmail.com>
2023-09-13 23:26:14 +0530

    optimized homepage

--- src/components/herosection.tsx
+++ src/components/herosection.tsx
@@ -19,6 +19,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import mission from "../../public/mission.svg";
 
 import { Separator } from "./ui/separator";
 import cert from "../../public/certificate.svg";
@@ -145,95 +146,135 @@ export default function herosection() {
       </div>
 
       {/* STATS  */}
-
-      <div className="container px-10 my-8 py-14">
+      <div className="container px-10 py-8 my-8">
         <h1 className="text-3xl md:text-5xl">
           A cutting-edge talent solutions organisation with
           <span className="text-transparent bg-gradient-to-r from-purple-700 via-purple-500 to-pink-500 bg-clip-text">
-            {" "}
-            10+ years of experience{" "}
+            10+ years of experience
           </span>
           , global reach, and a team of seasoned experts, ready to empower the
           next gen.
         </h1>
-        <div className="flex flex-col flex-wrap items-start justify-center w-full gap-2 pt-8 mx-auto md:flex-row md:gap-20 md:flex-nowrap">
-          <div className="w-[180px] max-w-[200px] flex flex-col gap-2">
+        <div className="flex flex-wrap items-start w-full gap-2 pt-16 mx-auto md:gap-20 md:flex-nowrap">
+          <div className=" max-w-[200px] flex flex-col gap-2">
             <Separator className="bg-[#252424] md:block" />
             <h3 className="font-bold">6,800+</h3>
             <p className="text-gray-600">
               Diverse talented individuals with a global presence
             </p>
           </div>
-          <div className="w-[180px] flex flex-col gap-2">
+          <div className="flex flex-col w-1/2 gap-2 lg:w-1/4 ">
             <Separator className="bg-[#252424] md:block" />
             <h3 className="font-bold">8+</h3>
             <p>Global presence in India, USA, UK, and Poland</p>
           </div>
-          <div className="w-[180px] flex flex-col gap-2">
+          <div className="flex flex-col w-1/2 gap-2 lg:w-1/4 ">
             <Separator className="bg-[#252424] md:block" />
             <h3 className="font-bold">8+</h3>
             <p>Healthcare, IT and BFSI industry</p>
           </div>
-
-          <div className="w-[180px] flex flex-col gap-2">
+          <div className="flex flex-col w-1/2 gap-2 lg:w-1/4 ">
             <Separator className="bg-[#252424] md:block" />
             <h3 className="font-bold">200+</h3>
             <p>Years of combined experience in talent management</p>
           </div>
         </div>
-        {/* Annual Report  */}
-        <div className="bg-[#FF008E] w-full rounded-lg container mt-4">
-          <div className="flex flex-col-reverse gap-6 px-10 py-8 md:flex-row">
-            <div className="flex flex-col gap-4 text-white">
-              <p>Annual report</p>
-              <h2 className="text-xl md:text-3xl ">
-                Experience the Scalene difference -
-              </h2>
-              <Button>
-                Global Hiring Index 2022
+      </div>
+      {/* Mission  */}
+      <div className="container flex flex-col gap-6 px-10 py-8 my-8 ">
+        <h3 className="text-sm font-bold text-[#FF008E]">Mission</h3>
+        <div className="flex py-6">
+          <div className="flex flex-col justify-between flex-[8] pr-6">
+            <h2 className="text-3xl md:text-4xl">Empowering Individuals</h2>
+            <p>
+              A cutting-edge talent solutions organization with a 10+ years of
+              experience, global reach, and a team of seasoned experts, ready to
+              empower the next generation.
+            </p>
+            <Link href="/about">
+              <div className="flex flex-row opacity-75">
+                Learn More
                 <ChevronRight />
-              </Button>
+              </div>
+            </Link>
+          </div>
+          <div className="flex-[6] ">
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <TableCell>Mission</TableCell>
+                  <TableCell>People</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Exports</TableCell>
+                  <TableCell>Partners</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Clients</TableCell>
+                  <TableCell>Stories</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      </div>
+      <div className="container flex flex-wrap items-center justify-center gap-6 px-10 py-8 my-8 md:flex-nowrap">
+        <div className="">
+          <Image
+            src={mission}
+            width="770"
+            height="433"
+            alt="mission"
+            className="max-w-[570px] w-[400px] min-w-[400px]"
+          />
+        </div>
+        <div className="flex flex-col items-start justify-between pl-20">
+          <h3 className="text-lg font-medium ">Our Story</h3>
+          <p>This is the reason of description is mandatory</p>
+          <Link href="/about">
+            <div className="flex flex-row ">
+              Learn More
+              <ChevronRight />
             </div>
-            <div>
-              <Image src={cert} width="524" height="295" alt="certificate" />
+          </Link>
+          <Separator />
+          <div className="opacity-70">
+            <div className="py-4">
+              <h3 className="text-base font-medium ">The global impact</h3>
+              <p className="text-sm">
+                This is the reason of description is mandatory
+              </p>
+            </div>
+            <div className="py-4">
+              <h3 className="text-base font-medium">Making sense of self</h3>
+              <p className="text-sm">
+                This is the reason of description is mandatory
+              </p>
+            </div>
+            <div className="py-4">
+              <h3 className="text-base font-medium">Making sense of self</h3>
+              <p className="text-sm">
+                This is the reason of description is mandatory
+              </p>
             </div>
           </div>
         </div>
-        {/* Mission  */}
-        <div className="">
-          <h3 className="text-sm font-bold">Mission</h3>
-          <div className="flex ">
-            <div>
-              <h2 className="text-3xl md:text-5xl">Empowering Individuals</h2>
-              <p>
-                A cutting-edge talent solutions organization with a 10+ years of
-                experience, global reach, and a team of seasoned experts, ready
-                to empower the next generation.
-              </p>
-              <div className="flex flex-row ">
-                <Link href="/about">
-                  Learn More <ChevronRight />
-                </Link>
-              </div>
-            </div>
-            <div>
-              <Table>
-                <TableBody>
-                  <TableRow>
-                    <TableCell>Mission</TableCell>
-                    <TableCell>People</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>Exports</TableCell>
-                    <TableCell>Partners</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>Clients</TableCell>
-                    <TableCell>Stories</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </div>
+      </div>
+      {/* Annual Report  */}
+      <div className="bg-[#FF008E] w-full rounded-lg container my-16">
+        <div className="flex flex-col-reverse gap-6 px-10 py-8 md:flex-row">
+          <div className="flex flex-col justify-around gap-4 text-white">
+            <p>Annual report</p>
+            <h2 className="text-xl md:text-3xl ">
+              Experience the Scalene difference -
+            </h2>
+            <Button>
+              Global Hiring Index 2022
+              <ChevronRight />
+            </Button>
+          </div>
+          <div>
+            <Image src={cert} width="524" height="295" alt="certificate" />
           </div>
         </div>
       </div>
